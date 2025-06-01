913958bb2d54fb7dcf36e3e82bb2fdbefe8fb4a9
ashishd881 <ashishkpdh881@gmail.com>
2025-06-01 07:45:52 +0530

    backend Coded

---
+++ server/models/userModel.js
@@ -0,0 +1,10 @@
+// import mongoose from "mongoose"
+
+// const userSchema =new mongoose.Schema({
+//     name:{type:String, required:true},
+//     email:{type:String, required:true,unique:true},
+//     password:{type:String,required:true},
+//     creditBalanace:{type:Number,default:5},
+// })
+
+// const userModel = mongoose.model.()
---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'  //extension ke saath likho
+const PORT = process.env.PORT || 4000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+await connectDB()  //connect express app with mongo db database
+app.get('/',(req,res)=> res.send("API Working"))
+
+app.listen(PORT,()=>console.log('Server running on Port' + PORT));
